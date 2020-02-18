61eb65761e8af03730ee09f6496534844ff980c6
keanan <32620736+KeananChetty@users.noreply.github.com>
2020-02-18 16:32:44 +0200

    Add files via upload

---
+++ VehicleDealership/VehicleDealership/VehicleLogic/CarLogic.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using VehicleDealership.Vehicle;
+
+namespace VehicleDealership.VehicleLogic
+{
+    public class CarLogic
+    {
+        public static List<Cars> cars = GetCars();
+
+        public static List <Cars> GetCars()
+        {
+            cars = new List<Cars>
+            {
+                /*new Cars (150000, 14569567, VehicleSpecs.High, VehicleColor.Flat , ServiceHistory.FullServiceHistory , "318i", 2014, true),
+                new Cars (150000, 14569567, VehicleSpecs.High, VehicleColor.Flat , ServiceHistory.FullServiceHistory , "318i", 2014, true)*/
+            };
+            return cars;
+        }
+
+        public static void DisplayCars()
+        {
+            foreach (var item in cars)
+            {
+                Console.WriteLine(
+                    
+                    $"\n Book Value of the car is: {item.BookValue}" +
+                    $"\n Millege On the Vehicle is: {item.Millage}" +
+                    $"\n The Spec the Vehicle is: {item.VehicleSpecs}" +
+                    $"\n The color the Vehicle is: {item.VehicleColor}" +
+                    $"\n Service History: {item.ServiceHistory}" +
+                    $"\n Vehicle Model: {item.VehicleSeries}" +
+                    $"\n The Year it was Produced: {item.VehicleYearProduced}" +
+                    $"\n-----------------------------------------------");
+            }
+        }
+    }
+}
---
+++ VehicleDealership/VehicleDealership/VehicleLogic/NewVehiclePrice.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VehicleDealership.VehicleLogic
+{
+    public class NewVehiclePrice
+    {
+        const int fullPercentage = 100;
+        int carNewValue;
+        const int LowMillage = 100000;
+        const int LowMillegeAmount = 30000;
+        const int MediamMillege = 150000;
+        const int MediamMillegeAmount = 5000;
+        const int YearLessThen20111 = 50000;
+        const int yearBetween2011And2018 = 10000;
+        const int YearGraterThen2019 = 30000;
+        public int Millage(int Bookvalue, int millage/*, VehicleSpecs vehicleSpecs, ServiceHistory serviceHistory, int year*/)
+        {
+            if (millage < LowMillage)
+            {
+                Bookvalue += LowMillegeAmount;
+            }
+            return Bookvalue;
+        }
+
+        public int VehicleSpec(int bookvalue, VehicleSpecs vehicleSpecs)
+        {
+            if (vehicleSpecs == VehicleSpecs.High)
+            {
+                bookvalue += (bookvalue / fullPercentage) * (int)VehicleSpecs.High;
+            }
+            else if (vehicleSpecs == VehicleSpecs.Mediam)
+            {
+                bookvalue += (bookvalue / fullPercentage) * (int)VehicleSpecs.Mediam;
+            }
+            return bookvalue;
+        }
+
+        public int ServiceHistory()
+        {
+
+        }
+    }
+}
